e72f8f12834cc9fcbdf4d1cba4fce4435394bcf6
kocian <>
2007-03-28 22:53:19 +0000

    added mainpage.h

---
+++ src/mainpage.h
@@ -0,0 +1,20 @@
+/** @mainpage package configData
+
+  @section intro Introduction
+  This package defines classes that hold LAT configuration information
+  for the processed event.
+  
+  This package contains:
+  - TrgConfig
+
+  <hr>
+  @section notes release notes
+  release.notes
+  <hr>
+  @section requirements requirements
+  @verbinclude requirements
+
+  @authors Martin Kocian and Eric Charles
+*/
+
+
