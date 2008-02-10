6336f3652b575126cd29b549080eec7e7cacbed2
cvs2git <>
2008-02-10 20:52:24 +0000

    This commit was manufactured by cvs2svn to create tag 'configData-01-06-04'.
    Sprout from master 2008-02-09 23:09:56 UTC panetta 'AutoUpdate'
    Cherrypick from master 2008-02-10 20:52:23 UTC kocian 'account for new latc_DFT_TRG_GEM files':
        src/db/TrgConfigDB.cxx
    Delete:
        python/.cvsignore
        python/AcdPlotChecker.py
        python/AcdXmlReport.py
        python/BasicPlotChecker.py
        python/BasicRootMacroRunner.py
        python/CalPrecinctReport.py
        python/CalXmlReport.py
        python/ConfigReport.py
        python/ConfigXmlReport.py
        python/Config_check.C
        python/Config_check.h
        python/LciXmlReport.py
        python/PrecinctReport.py
        python/RootRptGenerator.py
        python/TimingXmlReport.py
        python/TkrRegisterChecker.py
        python/TkrXmlReport.py
        python/TrgGemXmlReport.py
        python/calConstant.py
        python/configTransform.xsl
        python/dumpPrecinct.C
        python/tkrUtils.py

--- src/db/TrgConfigDB.cxx
+++ src/db/TrgConfigDB.cxx
@@ -7,21 +7,28 @@ TrgConfigDB::TrgConfigDB(LatcDB* lc):m_lc(lc),m_key(0),m_allowMissing(false){}
 
 bool TrgConfigDB::updateKey(const unsigned int key){
   int retval=false;
-  bool newStyleConfig = false;
   if (key!=m_key){
+    TrgConfigParser p(m_allowMissing);
     retval=true;
     std::string bcast=m_lc->getFilename("latc_DFT",key);
-    if (bcast.length() == 0){ // New style config 
+    if (bcast.length() != 0){ // Old style config 
+      int error=p.parse(this,bcast.c_str());
+      std::cout<<bcast<<std::endl;
+      assert(error==0);
+    } else { // new style configuration
+      bcast = m_lc->getFilename("latc_DFT_TRG_GEM", key); //this one may or may not exist
+      if (bcast.length()!=0){
+        int error = p.parse(this, bcast.c_str());
+        std::cout<<bcast<<std::endl;
+        assert(error==0);
+      }
       bcast = m_lc->getFilename("latc_GEM_TRG_GEM", key);
-      newStyleConfig = true;
-    }
-    std::cout<<bcast<<std::endl;
-    TrgConfigParser p(m_allowMissing);
-    int error=p.parse(this,bcast.c_str());
-    assert(error==0);
-    if (newStyleConfig) {
+      int error=p.parse(this,bcast.c_str());
+      std::cout<<bcast<<std::endl;
+      assert(error==0);
       bcast = m_lc->getFilename("latc_GEM_TRG_ROI", key);
       error = p.parse(this, bcast.c_str());
+      std::cout<<bcast<<std::endl;
       assert(error==0);
     }
     m_key=key;
