2132f0c12b3017ac6645bbfd3a734d58b608e89a
echarles <>
2008-05-30 01:36:56 +0000

    added some stuff to MootDBImpl to get OBF prescalers and top level MOOT key

--- configData/db/MootDBImpl.h
+++ configData/db/MootDBImpl.h
@@ -4,22 +4,29 @@
 
     Implementation to retrieve sets of files from MOOT
 
-    $Id: MootDBImpl.h,v 1.2 2007/12/07 02:13:34 kocian Exp $
+    $Id: MootDBImpl.h,v 1.1 2008/01/31 19:39:06 panetta Exp $
 */
 #ifndef MOOTDBIMPL_H
 #define MOOTDBIMPL_H
 
 #include "configData/db/LatcDB.h"
 #include <string>
+#include <map>
 #include "mootCore/MootQuery.h"
 #include "mootCore/FileDescrip.h"
+#include "enums/Lsf.h"
+
+class FswEfcSampler;
 
 using namespace MOOT;
 
 class MootDBImpl:public LatcDB{
- public:
+public:
   /// Default constructor
   MootDBImpl();
+
+  /// D'tor constructor
+  virtual ~MootDBImpl();
 
   /// function to retrieve filename of type type given key key.
   const std::string getFilename(const char* type, unsigned key);
@@ -27,7 +34,37 @@ class MootDBImpl:public LatcDB{
   /// function to retrieve the list of filenames given a MOOT key
   const std::vector<std::string> getFilenameList(unsigned key);
 
- private:
+  /// function to get the RunSummaryInfo and the MOOT master key
+  /// given the run start time and the source ID
+  AcqSummaryInfo* getAcqSummary(unsigned startedAt,
+				unsigned scid, 
+				unsigned& configKey) const;
+  
+  /// function to get the prescale factor
+  /// given the MOOT master key
+  unsigned prescaleFactor(unsigned configKey,			  			  
+			  MOOT::LpaMode lpaMode,
+			  unsigned handlerId,			  
+			  unsigned filterStatusWord,
+			  enums::Lsf::RsdState rsdState,
+			  enums::Lsf::LeakedPrescaler leakPrescaler) const;
+
+  bool readEfcFromFile( MOOT::LpaMode lpaMode, unsigned handlerId, const char* fileName );
+  
+protected:
+
+  void clearCache() const;
+  
+  FswEfcSampler* getFswCdb(unsigned configKey, MOOT::LpaMode lpaMode, unsigned handlerId) const;
+  
+private:
+  
+  mutable std::map<unsigned,FswEfcSampler*>       m_fswEfcCache;
+  mutable AcqSummaryInfo*                         m_acqSummaryCache;
+  mutable unsigned m_configKeyCache;
+  mutable unsigned m_startedAtCache;
+  mutable unsigned m_scidCache;
+
   MootQuery* m_mq;
 };
 
--- src/db/MootDbImpl.cxx
+++ src/db/MootDbImpl.cxx
@@ -1,13 +1,25 @@
 
 #include "configData/db/MootDBImpl.h"
+#include "configData/fsw/FswEfcSampler.h"
 #include "mootCore/FileDescrip.h"
 #include <vector>
 #include <iostream>
+#include <set>
+#include "enums/Lsf.h"
+#include "facilities/Util.h"
 
 
 MootDBImpl::MootDBImpl(){
   m_mq=new MootQuery;
+  clearCache();
 }
+
+
+MootDBImpl::~MootDBImpl() {
+  delete m_mq;  
+  clearCache();
+}
+
 
 // Local function to generate a list of ParmInfo from an unsigned MOOT key
 bool _cfgParmsFromKey(const unsigned configKey, 
@@ -63,4 +75,117 @@ const std::vector<std::string> MootDBImpl::getFilenameList(unsigned key){
   }
   //std::cout<<rs<<std::endl;
   return rv;
+}
+
+
+MOOT::AcqSummaryInfo* MootDBImpl::getAcqSummary(unsigned startedAt,
+						unsigned scid,
+						unsigned& configKey ) const {
+  if ( startedAt == m_startedAtCache &&
+       scid == m_scidCache ) {
+    configKey = m_configKeyCache;
+    return m_acqSummaryCache;
+  }
+  clearCache();
+  m_acqSummaryCache = m_mq->getAcqSummaryInfo(startedAt,scid);
+  if ( m_acqSummaryCache == 0 ) {    
+    configKey = LSF_INVALID_UINT;
+    return 0;
+  }
+  try {    
+    m_configKeyCache = facilities::Util::stringToUnsigned(m_acqSummaryCache->getConfigKey());
+  } catch ( ... ) {
+    m_acqSummaryCache = 0;
+    m_configKeyCache = LSF_INVALID_UINT;
+  }
+  return m_acqSummaryCache;
+}
+
+unsigned MootDBImpl::prescaleFactor(unsigned configKey,
+				    MOOT::LpaMode lpaMode,
+				    unsigned handlerId,
+				    unsigned filterStatusWord,
+				    enums::Lsf::RsdState rsdState,
+				    enums::Lsf::LeakedPrescaler leakedPrescaler) const {
+
+  FswEfcSampler* sampler = getFswCdb(configKey,lpaMode,handlerId);
+  if ( sampler == 0 ) return LSF_INVALID_UINT;
+
+  switch ( rsdState ) {
+  case enums::Lsf::VETOED:
+    return 0;
+  case enums::Lsf::PASSED:
+    return 1;
+  case enums::Lsf::IGNORED:
+    return sampler->inputPrescaler();
+  case enums::Lsf::INVALID:
+    return LSF_INVALID_UINT;
+  case enums::Lsf::SUPPRESSED:
+  case enums::Lsf::LEAKED:
+    if ( leakedPrescaler >= 0 ) { return sampler->prescaler(leakedPrescaler); }
+    else if ( leakedPrescaler == enums::Lsf::OUTPUT ) { return sampler->outputPrescaler(); }
+    else if ( leakedPrescaler == enums::Lsf::INPUT ) { return sampler->inputPrescaler(); }
+    break;
+  }
+  // try and scan the filter status word
+  unsigned checkBits = filterStatusWord & sampler->enabled();
+  for ( int i(31); i >= 0; i-- ) {
+    if (  checkBits & ( 1 << i ) ) return sampler->prescaler(i);
+  }
+  return 0; 
+}
+
+
+bool MootDBImpl::readEfcFromFile( MOOT::LpaMode lpaMode, unsigned handlerId, const char* fileName ) {
+  FswEfcSampler* sampler = FswEfcSampler::makeFromXmlFile(fileName);
+  if ( sampler == 0 ) return false;
+  if ( lpaMode == LPA_MODE_ALL ) {
+    for ( int mode = MOOT::LPA_MODE_NORMAL; mode < MOOT::LPA_MODE_count; mode++ ) {
+      unsigned mapKey = FswEfcSampler::mapKey(mode,handlerId);
+      m_fswEfcCache[mapKey] = sampler;
+    } 
+  } else {
+    unsigned mapKey = FswEfcSampler::mapKey(lpaMode,handlerId);
+    m_fswEfcCache[mapKey] = sampler;
+  }
+  return true;
+}
+
+
+void MootDBImpl::clearCache() const {
+  std::set<FswEfcSampler*> deleted;
+  for ( std::map<unsigned,FswEfcSampler*>::iterator itr = m_fswEfcCache.begin(); 
+	itr != m_fswEfcCache.end(); itr++ ) {
+    if ( deleted.find(itr->second) == deleted.end() ) {
+      deleted.insert(itr->second);
+      delete itr->second;
+    }
+  }
+  m_fswEfcCache.clear();
+  delete m_acqSummaryCache;
+  m_acqSummaryCache = 0;
+  m_configKeyCache = LSF_INVALID_UINT;
+  m_startedAtCache = LSF_INVALID_UINT;
+  m_scidCache = LSF_INVALID_UINT;
+}
+  
+FswEfcSampler* MootDBImpl::getFswCdb(unsigned configKey, MOOT::LpaMode lpaMode, 
+				     unsigned handlerId) const {
+  if ( configKey != m_configKeyCache ) {
+    return 0;
+  } 
+  unsigned mapKey = FswEfcSampler::mapKey(lpaMode,handlerId);
+
+  std::map<unsigned,FswEfcSampler*>::const_iterator itr = m_fswEfcCache.find(mapKey);
+  if ( itr != m_fswEfcCache.end() ) return itr->second;
+  std::string handlerName;
+  const ConstitInfo* fswConstit = m_mq->getActiveFilter(configKey,lpaMode,handlerId,handlerName);
+  if ( fswConstit == 0 ) return 0;
+
+  std::string xmlFilePath = std::string(getenv("MOOT_ARCHIVE"))+"/";
+  xmlFilePath += fswConstit->getSrcPath();
+  FswEfcSampler* sampler = FswEfcSampler::makeFromXmlFile(xmlFilePath.c_str());
+  if ( sampler == 0 ) return 0;
+  m_fswEfcCache[mapKey] = sampler;
+  return sampler;
 }
