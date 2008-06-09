f0a0aa2bd93dc128eccec4891dae5ffa446933ae
panetta <>
2008-06-09 23:46:12 +0000

    Add blank Associate and Ignore reports

---
+++ python/AssociateXmlReport.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+#
+#                               Copyright 2008
+#                                     by
+#                        The Board of Trustees of the
+#                     Leland Stanford Junior University.
+#                            All rights reserved.
+#
+
+__facility__ = "Online"
+__abstract__ = "Timing config reporting classes"
+__author__   = "P.A.Hart <philiph@SLAC.Stanford.edu> SLAC - GLAST LAT I&T/Online"
+__date__     = "2008/01/25 00:00:00"
+__updated__  = "$Date: 2008/02/11 22:04:40 $"
+__version__  = "$Revision: 1.5 $"
+__release__  = "$Name:  $"
+__credits__  = "SLAC"
+
+import logging, os
+
+from ConfigXmlReport import *
+
+class AssociateXmlReport(PrecinctXmlReport):
+    def __init__(self, precinctInfo, configData):
+        PrecinctXmlReport.__init__(self, precinctInfo, configData)
+        
+    def createReport(self, rebuild=False):
+        self.createHeader()
+        summary = self.addSection("%s_Summary" %(self.info.getPrecinct()))
+
+        self.addIntent(summary)  # blank intent node for later?
+
+
+    
+            
