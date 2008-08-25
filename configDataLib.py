0b201e4480b47c93b22147a1ecb5932778f62549
ecephas <>
2008-08-25 20:26:16 +0000

    Added the following <package>Lib calls:       env.Tool('mootCoreLib')       env.Tool('commonRootDataLib')

--- configDataLib.py
+++ configDataLib.py
@@ -1,7 +1,8 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/configData/configDataLib.py,v 1.1 2008/08/15 21:42:38 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['configData'])
-
+    env.Tool('mootCoreLib')
+    env.Tool('commonRootDataLib')
 def exists(env):
     return 1;
