fb569d58ec48ef4395a4bfda61a76b7706f5a252
ecephas <>
2008-12-09 00:08:43 +0000

    Check in configDataLib.py for configData v1r10p2 (branch-tag, GlastReleasev15r49).

--- configDataLib.py
+++ configDataLib.py
@@ -1,8 +1,11 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/configData/configDataLib.py,v 1.1 2008/08/15 21:42:38 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/configData/configDataLib.py,v 1.2 2008/08/25 20:26:16 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['configData'])
     env.Tool('mootCoreLib')
     env.Tool('commonRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['pythonLibs'])
+    env.Tool('lsfDataLib')
 def exists(env):
     return 1;
