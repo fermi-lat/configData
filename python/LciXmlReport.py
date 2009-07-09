c3ede55b4bd3ee86e150751df3e6f5a51298cbb5
panetta <>
2009-07-09 21:03:43 +0000

    Add LCI parameter file (LCI script) to LCI report

--- python/LciXmlReport.py
+++ python/LciXmlReport.py
@@ -11,8 +11,8 @@ __facility__ = "Online"
 __abstract__ = "MOOT config reporting base classes"
 __author__   = "J. Panetta <panetta@SLAC.Stanford.edu> SLAC - GLAST LAT I&T/Online"
 __date__     = "2008/01/25 00:00:00"
-__updated__  = "$Date: 2008/06/09 23:43:20 $"
-__version__  = "$Revision: 1.2 $"
+__updated__  = "$Date: 2008/06/13 18:54:27 $"
+__version__  = "$Revision: 1.4 $"
 __release__  = "$Name:  $"
 __credits__  = "SLAC"
 
@@ -29,8 +29,26 @@ class LciXmlReport(PrecinctXmlReport):
     def createReport(self, rebuild=False):
         logging.debug("Creating report for %s" % self.info.getPrecinct())
         self.createHeader()
-        summary = self.addSection("LCI_File")
+        voteSec = self.addSection("LCI_Vote")
         lciFile = os.path.join(os.environ['MOOT_ARCHIVE'],self.info.getSrc())
-        self.includeText(summary, lciFile)
-        self.addComment(summary, "empty comment")
+        self.includeText(voteSec, lciFile)
 
+        parmSec = self.addSection("LCI Parameter file")
+        parm = [p for p in self.info.parameters if 'lci' in p.getClass()][0] 
+        parmFile = os.path.join(os.environ['MOOT_ARCHIVE'], parm.getSrc())
+        self.includeText(parmSec, parmFile)
+
+
+if __name__ == '__main__':
+    from ConfigReport import *
+    configKey = 2527
+    lciVoteKey = 713
+    cr = ConfigDataHolder(configKey, None, './')
+    pInfo = cr.precinctInfo['CAL_LCI']
+    pInfo.alias = "cal_calibGen_flt"
+
+    report = LciXmlReport(pInfo, cr)
+    report.createReport()
+    xml = report.writeReport()
+
+    transformToFile(CONFIG_XSL_TRANSFORM, xml, xml[:-3]+'html')
