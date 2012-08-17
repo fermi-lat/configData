3e6c7fe674742ff614290114cee5ba8ff9fdfa8b
jrb <>
2012-08-17 18:42:39 +0000

    patches for Windows no-install-headers

--- configDataLib.py
+++ configDataLib.py
@@ -1,11 +1,15 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/configData/configDataLib.py,v 1.2 2008/08/25 20:26:16 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/configData/configDataLib.py,v 1.4 2009/01/23 00:20:54 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['configData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'configData') 
     env.Tool('mootCoreLib')
     env.Tool('commonRootDataLib')
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['pythonLibs'])
     env.Tool('lsfDataLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
 def exists(env):
     return 1;
